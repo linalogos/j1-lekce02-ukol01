4463608ade5f94517a2b79e0ae21ff213ef4a761
Elina Logozinskaya <esav903@gmail.com>
2022-04-17 20:29:04 +0200

    Doplneni obrazku jmenem.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -2,16 +2,14 @@ package net.sevecek.turtle;
 
 import net.sevecek.turtle.engine.*;
 
+import java.awt.*;
+
 public class HlavniProgram {
 
     public void main(String[] args) {
         Turtle zofka;
 
         zofka = new Turtle();
-
-        //nakresliPrasatko(zofka);
-
-        //zofka.setLocation(200,350);
 
         //nakresliOsmiuhelnik(zofka);
 
@@ -21,9 +19,13 @@ public class HlavniProgram {
 
         zofka.setLocation(60, 60);
 
+        zofka.setPenColor(Color.YELLOW);
+
         nakresliSlunicko (zofka);
 
         zofka.setLocation(80, 600);
+
+        zofka.setPenColor(Color.ORANGE);
 
         nakresliDomecek(zofka);
 
@@ -49,15 +51,96 @@ public class HlavniProgram {
 
         zofka.setLocation(370, 500);
 
+        zofka.setPenColor(Color.PINK);
+
         nakresliPrasatko(zofka);
 
+        zofka.setLocation(350, 100);
+
+        zofka.setPenColor(Color.CYAN);
+
+        nakresliE(zofka);
+
+        zofka.setLocation(400, 100);
+
+        nakresliL(zofka);
+
+        zofka.setLocation(450, 100);
+
+        nakresliI(zofka);
+
+        zofka.setLocation(470, 100);
+
+        nakresliN(zofka);
+
+        zofka.setLocation(540,100);
+
+        nakresliA(zofka);
 
 
+    }
 
+    private void nakresliA(Turtle zofka) {
+        zofka.turnRight(30);
+        zofka.move(90);
+        zofka.turnRight(120);
+        zofka.move(90);
+        zofka.turnLeft(180);
+        zofka.move(45);
+        zofka.turnLeft(60);
+        zofka.move(50);
+        zofka.turnRight(180);
+        zofka.move(50);
+        zofka.turnRight(60);
+        zofka.move(45);
+        zofka.turnLeft(150);
+    }
 
+    private void nakresliN(Turtle zofka) {
+        zofka.move(70);
+        zofka.turnRight(135);
+        zofka.move(90);
+        zofka.turnLeft(135);
+        zofka.move(70);
+        zofka.turnLeft(180);
+        zofka.move(70);
+        zofka.turnLeft(180);
+    }
 
+    private void nakresliI(Turtle zofka) {
+        zofka.move(70);
+        zofka.turnLeft(180);
+        zofka.move(70);
+        zofka.turnLeft(180);
+    }
 
+    private void nakresliL(Turtle zofka) {
+        zofka.move(70);
+        zofka.turnLeft(180);
+        zofka.move(70);
+        zofka.turnLeft(90);
+        zofka.move(35);
+        zofka.turnLeft(90);
+    }
 
+    private void nakresliE(Turtle zofka) {
+        zofka.turnLeft(135);
+        zofka.move(70);
+        zofka.turnRight(90);
+        zofka.move(35);
+        zofka.turnLeft(180);
+        zofka.move(35);
+        zofka.turnLeft(90);
+        zofka.move(35);
+        zofka.turnLeft(90);
+        zofka.move(35);
+        zofka.turnLeft(180);
+        zofka.move(35);
+        zofka.turnLeft(90);
+        zofka.move(35);
+        zofka.turnLeft(90);
+        zofka.move(35);
+        zofka.turnLeft(90);
     }
 
     private void nakresliDomecek(Turtle zofka) {
