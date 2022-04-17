2bd03ca282cb597df8e2b9adc9dbeb59b7b16bf2
Elina Logozinskaya <esav903@gmail.com>
2022-04-17 17:56:47 +0200

    Přidání metod nakresliPrasatko() a nakresliOsmiuhelnik().

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,69 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        nakresliPrasatko(zofka);
+
+        zofka.setLocation(200,350);
+
+        nakresliOsmiuhelnik(zofka);
+
+
+
+
+
+
+    }
+
+    private void nakresliOsmiuhelnik(Turtle zofka) {
+        zofka.turnLeft(45);
+        zofka.move(50);
+        for (int i = 0; i < 7; i++) {
+            zofka.turnRight(45);
+            zofka.move(60);
+        }
+    }
+
+    private void nakresliPrasatko(Turtle zofka) {
+        zofka.move(110);
+        zofka.turnRight(90);
+        zofka.move(130);
+        zofka.turnRight(90);
+        zofka.move(110);
+        zofka.turnRight(90);
+        zofka.move(130);
+        zofka.turnRight(45);
+        zofka.move(80);
+        zofka.turnRight(90);
+        zofka.move(80);
+        zofka.turnLeft(225);
+        zofka.move(110);
+        zofka.turnRight(45);
+        zofka.move(60);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnRight(90);
+        zofka.move(60);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnRight(135);
+        zofka.move(130);
+        zofka.turnRight(45);
+        zofka.move(60);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnLeft(90);
+        zofka.move(60);
+        zofka.turnRight(180);
+        zofka.move(60);
+        zofka.turnLeft(45);
+        zofka.move(55);
+        zofka.turnRight(90);
+        zofka.move(20);
+        zofka.turnLeft(45);
+        zofka.move(20);
+        zofka.turnRight(90);
+        zofka.move(20);
     }
 
 }
